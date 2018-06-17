79ea72f2c6890d2f2dd8aabfe42ca9533665ed46
Fongshway <kmf78@georgetown.edu>
2018-06-16 21:36:50 -0700

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import os
-import multiprocessing
-import sys
-from setuptools import setup, find_packages
 import uuid
+
+from setuptools import setup, find_packages
 
 f = open('README.rst')
 long_description = f.read().strip()
@@ -37,36 +36,33 @@ for category, filename in REQUIREMENTS_FILES.items():
             ]
     REQUIREMENTS[category] = requirements
 
-if sys.version_info < (2, 7):
-    REQUIREMENTS['test'].append('unittest2')
-    REQUIREMENTS['install'].append('ordereddict')
-
-setup(name='taskw',
-      version='1.2.0',
-      description="Python bindings for your taskwarrior database",
-      long_description=long_description,
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Intended Audience :: Developers",
-      ],
-      keywords='taskwarrior task',
-      author='Ralph Bean',
-      author_email='ralph.bean@gmail.com',
-      url='http://github.com/ralphbean/taskw',
-      license='GPLv3+',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=REQUIREMENTS['install'],
-      test_suite='nose.collector',
-      tests_require=REQUIREMENTS['test'],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+setup(
+    name='taskw',
+    version='1.2.0',
+    description="Python bindings for your taskwarrior database",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Intended Audience :: Developers",
+    ],
+    keywords='taskwarrior task',
+    author='Ralph Bean',
+    author_email='ralph.bean@gmail.com',
+    url='http://github.com/ralphbean/taskw',
+    license='GPLv3+',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS['install'],
+    test_suite='nose.collector',
+    tests_require=REQUIREMENTS['test'],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
